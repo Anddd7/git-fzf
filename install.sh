4254ebacc6def59c848ca15bd05bfc4105f30c10
Anddd7 <liaoad_space@sina.com>
2023-12-07 18:21:29 +0800

    fix: fix install folder

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 remote_url="https://raw.githubusercontent.com/Anddd7/git-fzf/main"
-local_folder="$HOME/.git-fzf"
+local_folder="$HOME/bin/git-fzf"
 
 mkdir -p "$local_folder"
 
 curl -sSL "$remote_url/git-fzf-commit" -o "$local_folder/git-fzf-commit"
 
-echo 'PATH="$HOME/.git-fzf:$PATH"' >>~/.zshrc
+echo 'PATH="$HOME/bin/.git-fzf:$PATH"' >>~/.zshrc
 
 echo 'alias gfzfc="git fzf-commit"' >>~/.zshrc
