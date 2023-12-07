848fd33e520d0066209371041369852835eb7d23
Anddd7 <liaoad_space@sina.com>
2023-12-07 18:02:15 +0800

    fix: rename and fix scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mdkir -p ~/git-fzf
+mkdir -p ~/.git-fzf
 
-curl -sSL "https://raw.githubusercontent.com/Anddd7/git-fzf/main/git-fzf-commit.sh" >~/git-fzf/git-fzf-commit.sh
+curl -sSL "https://raw.githubusercontent.com/Anddd7/git-fzf/main/git-fzf-commit" >~/.git-fzf/git-fzf-commit.sh
 
 echo 'PATH="$HOME/git-fzf:$PATH"' >>~/.zshrc
 
