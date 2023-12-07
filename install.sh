04d4cbd95fb74508709495954c91a4e4329dc3c7
Anddd7 <liaoad_space@sina.com>
2023-12-07 18:35:29 +0800

    fix: fix the permission

--- install.sh
+++ install.sh
@@ -7,8 +7,6 @@ mkdir -p "$local_folder"
 
 curl -sSL "$remote_url/git-fzf-commit" -o "$local_folder/git-fzf-commit"
 
-echo 'PATH="$HOME/bin/git-fzf:$PATH"' >>~/.zshrc
+echo 'export PATH="$HOME/bin/git-fzf:$PATH"' >>~/.zshrc
 
 echo 'alias gfzfc="git fzf-commit"' >>~/.zshrc
-
-source ~/.zshrc
